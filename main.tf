18287e67d9ff3c70e2b8ca94ce415d18625ecd01
Soren Martius <soeren.martius@gmail.com>
2020-02-19 21:31:24 +0100

    prototype a simpler version

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@
 
 locals {
   skip_zone_creation = length(local.zones) == 0
-  zones              = var.name == null ? [] : try(tolist(var.name), [tostring(var.name)], [])
+  zones              = try(tolist(var.name), [tostring(var.name)], [])
 
   delegation_set_id = var.delegation_set_id != null ? var.delegation_set_id : try(
     aws_route53_delegation_set.delegation_set[0].id, null
@@ -61,72 +61,43 @@ resource "aws_route53_zone" "zone" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 locals {
-  records_expanded = [
-    for record in var.records : merge({
-      record_id       = join("-", compact([lower(record.type), try(lower(record.name), ""), ]))
-      name            = ""
-      ttl             = null
-      records         = null
-      alias           = null
-      allow_overwrite = null
-      health_check_id = null
-    }, record)
-  ]
 
-  records_transposed = {
-    for record in local.records_expanded : record.record_id => record
+  records = {
+    for record in var.records : join("-", compact([lower(record.type), try(lower(record.name), ""), ])) => merge({
+      name = ""
+    }, record)
   }
 
   records_by_name = var.enable_module ? {
-    for product in setproduct(local.zones, keys(local.records_transposed)) : "${product[1]}-${product[0]}" => {
+    for product in setproduct(local.zones, keys(local.records)) : "${product[1]}-${product[0]}" => {
 
       # We need to wrap the reference to aws_route53_zone inside a try to avoid exceptations that might occur when we
       # run terraform destroy without running a successful terraform apply before.
       zone_id = try(aws_route53_zone.zone[product[0]].id, null)
 
-      name            = local.records_transposed[product[1]].name
-      type            = local.records_transposed[product[1]].type
-      allow_overwrite = local.records_transposed[product[1]].allow_overwrite
-      health_check_id = try(local.records_transposed[product[1]].health_check_id, null)
+      name            = local.records[product[1]].name
+      type            = local.records[product[1]].type
+      allow_overwrite = try(local.records[product[1]].allow_overwrite, false)
+      health_check_id = try(local.records[product[1]].health_check_id, null)
 
       # TTL conflicts with Alias records. If no alias is set, we should either use the ttl defined with the current
       # record or fall back to the default TTL that is configurable with var.default_ttl
-      ttl = local.records_transposed[product[1]].alias == null ? try(
-        local.records_transposed[product[1]].ttl == null ? var.default_ttl : local.records_transposed[product[1]].ttl,
+      ttl = can(local.records[product[1]].alias) == false ? try(
+        can(local.records[product[1]].ttl) == false ? var.default_ttl : local.records[product[1]].ttl,
         var.default_ttl
       ) : null
 
       # The DNS protocol has a 255 character limit per string. However, each TXT record can have multiple strings,
       # each 255 characters long. Hence, we split up the passed records for TXT records.
-      records = try([for record in local.records_transposed[product[1]].records :
-        local.records_transposed[product[1]].type == "TXT" && length(regexall("(\\\"\\\")", record)) == 0 ?
+      records = try([for record in local.records[product[1]].records :
+        local.records[product[1]].type == "TXT" && length(regexall("(\\\"\\\")", record)) == 0 ?
         join("\"\"", compact(split("{SPLITHERE}", replace(record, "/(.{255})/", "$1{SPLITHERE}")))) : record
       ], null)
 
-      alias = try(local.records_transposed[product[1]].alias, {})
+      alias = try(local.records[product[1]].alias, {})
     }
   } : {}
 
-  records_by_zone_id = {
-    for record in local.records_transposed : record.record_id => {
-      zone_id         = var.zone_id
-      name            = record.name
-      type            = record.type
-      allow_overwrite = record.allow_overwrite
-      health_check_id = try(record.health_check_id, null)
-
-      ttl = record.alias == null ? try(record.ttl == null ? var.default_ttl : record.ttl, var.default_ttl) : null
-
-      records = try([for r in record.records :
-        record.type == "TXT" && length(regexall("(\\\"\\\")", r)) == 0 ?
-        join("\"\"", compact(split("{SPLITHERE}", replace(r, "/(.{255})/", "$1{SPLITHERE}")))) : r
-      ], null)
-
-      alias = try(record.alias, {})
-    }
-  }
-
-  records = local.skip_zone_creation ? local.records_by_zone_id : local.records_by_name
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -134,7 +105,7 @@ locals {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_route53_record" "record" {
-  for_each = var.enable_module ? local.records : {}
+  for_each = var.enable_module ? local.records_by_name : {}
 
   zone_id         = each.value.zone_id
   type            = each.value.type
@@ -144,13 +115,13 @@ resource "aws_route53_record" "record" {
   records         = each.value.records
   health_check_id = each.value.health_check_id
 
-  dynamic "alias" {
-    for_each = each.value.alias == null ? [] : [each.value.alias]
-
-    content {
-      name                   = alias.value.name
-      zone_id                = alias.value.zone_id
-      evaluate_target_health = alias.value.evaluate_target_health
-    }
-  }
+  //  dynamic "alias" {
+  //    for_each = each.value.alias == null ? [] : [each.value.alias]
+  //
+  //    content {
+  //      name                   = alias.value.name
+  //      zone_id                = alias.value.zone_id
+  //      evaluate_target_health = alias.value.evaluate_target_health
+  //    }
+  //  }
 }
