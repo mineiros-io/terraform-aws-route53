0805d97319c70f1de3c8c3ec460023945ea5d666
Soren Martius <soeren.martius@gmail.com>
2020-07-22 08:04:03 +0200

    Add unit-disabled unit test

---
+++ test/unit-disabled/main.tf
@@ -0,0 +1,27 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# EMPTY FEATURES (DISABLED) UNIT TEST
+# This module tests an empty set of features.
+# The purpose is to verify no resources are created when the module is disabled.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+variable "aws_region" {
+  description = "(Required) The AWS region in which all resources will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+provider "aws" {
+  region  = var.aws_region
+  version = "~> 2.45"
+}
+
+module "test" {
+  source = "../.."
+
+  module_enabled = false
+}
+
+output "all" {
+  description = "All outputs of the module."
+  value       = module.test
+}
