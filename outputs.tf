a7b7e465a10eded6c7ccc65567856a51f8c181fb
Soren Martius <soeren.martius@gmail.com>
2021-07-26 13:09:30 +0100

    refactor: wrap outputs in try block and return empty map for delegation_set per default

--- outputs.tf
+++ outputs.tf
@@ -8,17 +8,17 @@
 
 output "zone" {
   description = "The created Hosted Zone(s)."
-  value       = aws_route53_zone.zone
+  value       = try(aws_route53_zone.zone, {})
 }
 
 output "records" {
   description = "A list of all created records."
-  value       = aws_route53_record.record
+  value       = try(aws_route53_record.record, {})
 }
 
 output "delegation_set" {
   description = "The outputs of the created delegation set."
-  value       = try(aws_route53_delegation_set.delegation_set[0], null)
+  value       = try(aws_route53_delegation_set.delegation_set[0], {})
 }
 
 # ------------------------------------------------------------------------------
